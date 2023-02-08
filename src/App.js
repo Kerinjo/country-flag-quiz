7d73750989201dcb93860f4ee2f3d6ead5690fae
Konrad Marciniak <konmarciniak@gmail.com>
2023-02-08 22:41:45 +0100

    now renders 4 random countries instead of all of them

--- src/App.js
+++ src/App.js
@@ -14,10 +14,17 @@ const App = () => {
     })   
   }, [])
 
+  const getRandomCountries = (countries, num) => {
+    const shuffled = [...countries].sort(() => 0.5 - Math.random())
+    return shuffled.slice(0, num)
+  }
+
+  const random_countries = getRandomCountries(countries, 4)
+
   return (
     <div>
       <ul>
-        {countries.map(country => 
+        {random_countries.map(country => 
           <li key={country.cca2}>
             {country.name.common}
             {country.flag}
